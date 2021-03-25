b461d3cd197360891e08febf9253a6b68f2fcd34
Laura.Barragan <Laura.Barragan@127.254.254.254>
2021-03-25 15:30:27 -0600

    Session Mar 25

--- src/main/java/com/nagarro/flight/search/api/controllers/FlightSearchController.java
+++ src/main/java/com/nagarro/flight/search/api/controllers/FlightSearchController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,7 @@ public class FlightSearchController {
         return "Hello " + name + "!";
     }
     
+    @CrossOrigin(origins="*")
     @RequestMapping(value="/flightSearch", method = RequestMethod.POST)
     public ResponseEntity<List<FlightInfo>> flightSearch(@RequestBody FlightInfoQuery query) {
         List<FlightInfo> results = new ArrayList<>();
